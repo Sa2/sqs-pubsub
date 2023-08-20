b448b05f2244919aff611154e8ee74ce560d9c41
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2023-08-20 09:25:39 +0900

    mod message

--- publisher/helloworld.go
+++ publisher/helloworld.go
@@ -3,11 +3,15 @@ package publisher
 import (
 	"Sa2/sqs-pubsub/sqsinterface"
 	"encoding/json"
+	"fmt"
+	"time"
 )
 
 func SetHelloWorldMessage() (sqsinterface.SQSMessage, error) {
+
+	now := time.Now()
 	param := sqsinterface.HelloWorldMessage{
-		Message: "from publisher",
+		Message: fmt.Sprintf("from publisher %v", now.Format("2006-01-02 15:04:05")),
 	}
 
 	paramJSON, err := json.Marshal(param)
